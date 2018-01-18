ba29b9177c9bb55dba0ed66e9d607487a8c6ebfa
gmoodyfootball <gmoodyfootball@gmail.com>
2018-01-18 09:42:53 -0700

    test 2

--- MoodyGMathQuiz/Form1.cs
+++ MoodyGMathQuiz/Form1.cs
@@ -17,7 +17,7 @@ namespace MoodyGMathQuiz
         Random randomizer = new Random();
 
         // Variables for the addition problem
-        // this is a change
+        // this is a change part duece
         public Form1()
         {
             InitializeComponent();
