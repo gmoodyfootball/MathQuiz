059ad33d32a7b10dc1a25439364e0aa11b237654
gmoodyfootball <gmoodyfootball@gmail.com>
2018-01-20 14:19:55 -0700

    Should be the final

--- MoodyGMathQuiz/Form1.Designer.cs
+++ MoodyGMathQuiz/Form1.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timeLabel = new System.Windows.Forms.Label();
-            this.timeLeft = new System.Windows.Forms.Label();
+            this.timeLeftLabel = new System.Windows.Forms.Label();
             this.plusLeftLabel = new System.Windows.Forms.Label();
             this.plusRightLabel = new System.Windows.Forms.Label();
             this.sumPlus = new System.Windows.Forms.Label();
@@ -51,6 +53,8 @@
             this.dividedLabelRight = new System.Windows.Forms.Label();
             this.dividedLabelLeft = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeDateLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.sum)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.difference)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.product)).BeginInit();
@@ -61,20 +65,20 @@
             // 
             this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timeLabel.Location = new System.Drawing.Point(272, 9);
+            this.timeLabel.Location = new System.Drawing.Point(272, 17);
             this.timeLabel.Name = "timeLabel";
             this.timeLabel.Size = new System.Drawing.Size(200, 30);
             this.timeLabel.TabIndex = 0;
             // 
-            // timeLeft
+            // timeLeftLabel
             // 
-            this.timeLeft.AutoSize = true;
-            this.timeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timeLeft.Location = new System.Drawing.Point(165, 10);
-            this.timeLeft.Name = "timeLeft";
-            this.timeLeft.Size = new System.Drawing.Size(101, 25);
-            this.timeLeft.TabIndex = 1;
-            this.timeLeft.Text = "Time Left";
+            this.timeLeftLabel.AutoSize = true;
+            this.timeLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeftLabel.Location = new System.Drawing.Point(165, 18);
+            this.timeLeftLabel.Name = "timeLeftLabel";
+            this.timeLeftLabel.Size = new System.Drawing.Size(101, 25);
+            this.timeLeftLabel.TabIndex = 1;
+            this.timeLeftLabel.Text = "Time Left";
             // 
             // plusLeftLabel
             // 
@@ -124,6 +128,8 @@
             this.sum.Name = "sum";
             this.sum.Size = new System.Drawing.Size(100, 35);
             this.sum.TabIndex = 2;
+            this.sum.ValueChanged += new System.EventHandler(this.check_answer_add);
+            this.sum.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // difference
             // 
@@ -132,6 +138,8 @@
             this.difference.Name = "difference";
             this.difference.Size = new System.Drawing.Size(100, 35);
             this.difference.TabIndex = 3;
+            this.difference.ValueChanged += new System.EventHandler(this.check_answer_minus);
+            this.difference.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // minusEqual
             // 
@@ -180,6 +188,8 @@
             this.product.Name = "product";
             this.product.Size = new System.Drawing.Size(100, 35);
             this.product.TabIndex = 4;
+            this.product.ValueChanged += new System.EventHandler(this.check_answer_multiply);
+            this.product.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // productEqual
             // 
@@ -228,6 +238,8 @@
             this.quotient.Name = "quotient";
             this.quotient.Size = new System.Drawing.Size(100, 35);
             this.quotient.TabIndex = 5;
+            this.quotient.ValueChanged += new System.EventHandler(this.check_answer_divide);
+            this.quotient.Enter += new System.EventHandler(this.answer_Enter);
             // 
             // quotientEqual
             // 
@@ -281,11 +293,28 @@
             this.startButton.UseVisualStyleBackColor = true;
             this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeDateLabel
+            // 
+            this.timeDateLabel.AutoSize = true;
+            this.timeDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.timeDateLabel.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.timeDateLabel.Location = new System.Drawing.Point(12, 19);
+            this.timeDateLabel.Name = "timeDateLabel";
+            this.timeDateLabel.Size = new System.Drawing.Size(48, 24);
+            this.timeDateLabel.TabIndex = 22;
+            this.timeDateLabel.Text = "Date";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(480, 358);
+            this.Controls.Add(this.timeDateLabel);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.quotient);
             this.Controls.Add(this.quotientEqual);
@@ -307,9 +336,10 @@
             this.Controls.Add(this.sumPlus);
             this.Controls.Add(this.plusRightLabel);
             this.Controls.Add(this.plusLeftLabel);
-            this.Controls.Add(this.timeLeft);
+            this.Controls.Add(this.timeLeftLabel);
             this.Controls.Add(this.timeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Math Quiz";
@@ -326,7 +356,7 @@
         #endregion
 
         private System.Windows.Forms.Label timeLabel;
-        private System.Windows.Forms.Label timeLeft;
+        private System.Windows.Forms.Label timeLeftLabel;
         private System.Windows.Forms.Label plusLeftLabel;
         private System.Windows.Forms.Label plusRightLabel;
         private System.Windows.Forms.Label sumPlus;
@@ -348,6 +378,8 @@
         private System.Windows.Forms.Label dividedLabelRight;
         private System.Windows.Forms.Label dividedLabelLeft;
         private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeDateLabel;
     }
 }
 
--- MoodyGMathQuiz/Form1.cs
+++ MoodyGMathQuiz/Form1.cs
@@ -17,7 +17,75 @@ namespace MoodyGMathQuiz
         Random randomizer = new Random();
 
         // Variables for the addition problem
-        // this is a change part duece
+        int addend1;
+        int addend2;
+        // subtraction
+        int minuend;
+        int subtrahend;
+        // Multiply
+        int multiplicand;
+        int multiplier;
+        //Divide
+        int dividend;
+        int divisor;
+
+        // Create a countdown timer
+        int timeLeft;
+
+        public void StartTheQuiz()
+        {
+            //create random numbers
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            //Display said numbers
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            //The starting value of the upDown counter
+            sum.Value = 0;
+
+            //Fill in the subtraction portion
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend); //so we don't have a negative number
+            minusLabelLeft.Text = minuend.ToString();
+            minusLabelRight.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            //Fill in the multiplication problem
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLabelLeft.Text = multiplicand.ToString();
+            timesLabelRight.Text = multiplier.ToString();
+            product.Value = 0;
+
+            //Fill in the division problem
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLabelLeft.Text = dividend.ToString();
+            dividedLabelRight.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            //Start the timer
+            timeLeft = 30; //30 seconds
+            timeLabel.Text = "30 Seconds";
+            timer1.Start();
+        }
+
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -30,12 +98,130 @@ namespace MoodyGMathQuiz
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //Set the time
+            DateTime today = new DateTime();
+            today = DateTime.Today;
+            timeDateLabel.Text = today.ToString("dd MMMM yyyy"); // 30 January 2018 format
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
-
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer()) //If the answers are all right
+            {
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!", "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                if (timeLeft <= 5)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+                //Display the new time left
+                //by updating the Time Left label
+                timeLeft -= 1; //Minus a second
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                //If time is up, stop the timer, show a MessageBox
+                //and fill in the answers
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time", "Sorry, you stink!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+
+                timeLabel.BackColor = default(Color);
+
+                startButton.Enabled = true;
+            }
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            //Select the whole answer in the NumbericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
+
+        private void check_answer_add(object sender, EventArgs e)
+        {
+            //Called whenever they change the answer in the add answerbox
+            if (addend1 + addend2 == sum.Value)
+            {
+                //play a sound or something
+                sum.BackColor = Color.LightGreen;
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+            else
+            {
+                sum.BackColor = default(Color);
+                //They're not right. So, they're wrong. No sound for you!
+            }
+        }
+
+        private void check_answer_minus(object sender, EventArgs e)
+        {
+            //Called whenever they change the answer in the minus answerbox
+            if (minuend - subtrahend == difference.Value)
+            {
+                //play a sound or something
+                difference.BackColor = Color.LightGreen;
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+            else
+            {
+                difference.BackColor = default(Color);
+                //They're not right. So, they're wrong. No sound for you!
+            }
+        }
+
+        private void check_answer_multiply(object sender, EventArgs e)
+        {
+            //Called whenever they change the answer in the multiply answerbox
+            if (multiplicand * multiplier == product.Value)
+            {
+                //play a sound or something
+                product.BackColor = Color.LightGreen;
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+            else
+            {
+                product.BackColor = default(Color);
+                //They're not right. So, they're wrong. No sound for you!
+            }
+        }
+
+        private void check_answer_divide(object sender, EventArgs e)
+        {
+            //Called whenever they change the answer in the multiply answerbox
+            if (dividend / divisor == quotient.Value)
+            {
+                //play a sound or something
+                quotient.BackColor = Color.LightGreen;
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+            else
+            {
+                quotient.BackColor = default(Color);
+                //They're not right. So, they're wrong. No sound for you!
+            }
+        }
+
     }
 }
